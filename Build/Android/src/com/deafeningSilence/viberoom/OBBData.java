7c3d4b7e4fc3ff4920f58252bba7e410186ec2a9
rkinabhi <rkinabhi@gmail.com>
2018-02-23 21:09:59 +0530

    all image formation cases on bench

--- Build/Android/src/com/deafeningSilence/viberoom/OBBData.java
+++ Build/Android/src/com/deafeningSilence/viberoom/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-175753580L // the length of the file in bytes
+175849577L // the length of the file in bytes
 )
 };
 };
