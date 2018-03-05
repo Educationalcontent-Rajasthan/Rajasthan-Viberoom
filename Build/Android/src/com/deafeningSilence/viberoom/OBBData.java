703bd6c5ac06e853793a7508912147dcc151f443
abhijeeth padarthi <rkinabhi@gmail.com>
2018-03-05 22:39:52 +0530

    fixed teleport to benches

--- Build/Android/src/com/deafeningSilence/viberoom/OBBData.java
+++ Build/Android/src/com/deafeningSilence/viberoom/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-59543052L // the length of the file in bytes
+59541574L // the length of the file in bytes
 )
 };
 };
