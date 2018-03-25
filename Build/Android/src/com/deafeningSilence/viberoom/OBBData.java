d507a9f2db6ecd6f00bd2661294de84bec20f4cb
abhijeeth padarthi <rkinabhi@gmail.com>
2018-03-25 11:19:15 +0530

    updated game dev credits and bench model

--- Build/Android/src/com/deafeningSilence/viberoom/OBBData.java
+++ Build/Android/src/com/deafeningSilence/viberoom/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-131528606L // the length of the file in bytes
+126903641L // the length of the file in bytes
 )
 };
 };
